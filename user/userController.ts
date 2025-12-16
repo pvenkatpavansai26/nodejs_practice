5182c8d820dc8caf46b967421627b004d2f6d87b
pvenkat26 <PVenkat@yathisolutions.com>
2025-12-16 13:14:00 +0530

    create user error handler

--- user/userController.ts
+++ user/userController.ts
@@ -1,6 +1,12 @@
 import type { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel.ts";
+import bcrypt from "bcrypt";
+import { sub } from "date-fns";
+import pkg from "jsonwebtoken";
+const { sign } = pkg;
+import type { User } from "./userTypes.ts";
+
 
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
@@ -12,19 +18,40 @@ if(!name || !email || !password){
     return next(error);
   } 
 //database call
+try{
+    const user = await userModel.findOne({email});
 
-const user = await userModel.findOne({email});
-if(user){
+    if(user){
     const error = createHttpError(409, "User already exists");
     return next(error);
   }
+}
+catch (error){
+    return next (createHttpError(500, "error"));
+}
 
 
-
-
-
-res.json({message: "User created"});
-
-
+//password
+const hashedPassword = await bcrypt.hash(password, 10);
+let newUser: User;
+try{
+    newUser = await userModel.create({
+    name,
+    email,
+    password: hashedPassword
+});
+}
+catch (error){
+    return next (createHttpError(500, "error"));
+}
+try{ 
+//token generator
+const token = sign({sub: newUser._id}, process.env.jwt_secret as string, {expiresIn: '1d'});
+//response
+res.json ({accessToken: token});
+}
+catch (error){
+    return next (createHttpError(500, "error"));
+}
 };
 export { createUser };
